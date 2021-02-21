52ab7d1b2fb20d542bb0af024527e1c83a6e0440
chantal2000 <nniyonkuruchantal@gmail.com>
2021-02-21 09:24:38 +0300

    first assignment

---
+++ src/Mobile development.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var FullName="Niyonkuru Marie Chantal"
+    println(FullName)
+    var Age=20
+    println(Age)
+    var PhoneNumber="0786738069"
+    println(PhoneNumber)
+    var weight=50
+    println(weight)
+    var citizen="Rwanda"
+    println(citizen)
+}
